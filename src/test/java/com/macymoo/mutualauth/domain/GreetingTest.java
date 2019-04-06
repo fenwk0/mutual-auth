5c927c43329dc218b44427f7d260f8ffd7e1b482
fenwk0 <karl.fenwick@gmail.com>
2019-04-06 23:26:03 +0100

    Added some true love tests

---
+++ src/test/java/com/macymoo/mutualauth/domain/GreetingTest.java
@@ -0,0 +1,14 @@
+package com.macymoo.mutualauth.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GreetingTest {
+
+    @Test
+    public void firstTest() {
+        assertTrue("This will succeed.", true);
+    }
+
+}
