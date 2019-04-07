ff68c1164e358f36627507ee9bb255ee3543c7f3
fenwk0 <karl.fenwick@gmail.com>
2019-04-07 11:05:50 +0100

    Moved the rest api to the application and fixed the property references, it works now.

--- src/main/java/com/macymoo/mutualauth/controller/GreetingController.java
+++ src/main/java/com/macymoo/mutualauth/controller/GreetingController.java
@@ -1,21 +1,16 @@
 package com.macymoo.mutualauth.controller;
 
 import com.macymoo.mutualauth.domain.Greeting;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController
+
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
-    }
+
 }
