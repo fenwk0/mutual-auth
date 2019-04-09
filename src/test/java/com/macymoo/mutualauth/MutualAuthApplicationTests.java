8e80fe1eb076f30831014488506610e5095ff447
fenwk0 <karl.fenwick@gmail.com>
2019-04-09 12:14:18 +0100

    Added generated trust store and key store to resources and referenced via class path, server now starts but get this error when establishing trust.
    MutualAuthApplicationTests.testGreeting: https://localhost:53544/greeting
    retVal = FailedPKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException: unable to find valid certification path to requested target for https://localhost:53544/greeting [Check that the url returns a response in your browser]
    [ERROR] Tests run: 2, Failures: 1, Errors: 0, Skipped: 0, Time elapsed: 5.431 s <<< FAILURE! - in com.macymoo.mutualauth.MutualAuthApplicationTests
    [ERROR] testGreeting(com.macymoo.mutualauth.MutualAuthApplicationTests)  Time elapsed: 0.606 s  <<< FAILURE!
    java.lang.AssertionError
    	at com.macymoo.mutualauth.MutualAuthApplicationTests.testGreeting(MutualAuthApplicationTests.java:35)

--- src/test/java/com/macymoo/mutualauth/MutualAuthApplicationTests.java
+++ src/test/java/com/macymoo/mutualauth/MutualAuthApplicationTests.java
@@ -1,17 +1,13 @@
 package com.macymoo.mutualauth;
 
-import com.macymoo.mutualauth.domain.Greeting;
 import com.macymoo.mutualauth.utils.RestApi;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
-import javax.validation.constraints.AssertTrue;
-
+import static org.assertj.core.api.Assertions.fail;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
@@ -31,11 +27,17 @@ public class MutualAuthApplicationTests {
 
     @Test
     public void testGreeting() {
-        String testUrl = "http://localhost:" + port + "/greeting";
-        System.out.println("MutualAuthApplicationTests.testGreeting: " + testUrl);
-        String retVal = restApi.invoke(testUrl);
-        System.out.println("retVal = " + retVal);
-        assertTrue(retVal.contains("Success: [200]\n" +
-                "{\"id\":1,\"content\":\"[greeting][0] Hello, World!\"}"));
+        String testUrl = "https://localhost:" + port + "/greeting";
+        try {
+            System.out.println("MutualAuthApplicationTests.testGreeting: " + testUrl);
+            String retVal = restApi.invoke(testUrl);
+            System.out.println("retVal = " + retVal);
+            assertTrue(retVal.contains("Success: [200]\n" +
+                    "{\"id\":1,\"content\":\"[greeting][0] Hello, World!\"}"));
+
+        } catch (Exception e) {
+            fail(e.getMessage() + testUrl);
+        }
     }
+
 }
